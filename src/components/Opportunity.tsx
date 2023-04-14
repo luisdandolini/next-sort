b3f7043bfb984aa7ae86ef90f6237e6229825019
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2023-04-14 17:44:36 -0300

    add dynamic

--- src/components/Opportunity.tsx
+++ src/components/Opportunity.tsx
@@ -1,11 +1,52 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import styles from "../styles/Opportunity.module.css";
+import api from "../services/api";
+
+interface Opportunity {
+  id: any; 
+  name: any;
+  zone: any;
+  zone_full: any;
+  suites: any;
+  price: any; 
+  city: any;
+  image: any;
+  product_media: Array<{ url: string }>; 
+}
+
 
 export default function Opportunity() {
+  const [opportunities, setOpportunities] = useState<Opportunity[]>([]);
+
+  useEffect(() => {
+    api
+      .get('/guest/products-best')
+      .then((response) => {
+        console.log(response);
+        const formattedData = response.data.map((item: Opportunity) => {
+          const imageUrl = item.product_media && item.product_media[0] ? item.product_media[0].url : '';
+          return {
+            ...item,
+            image: imageUrl,
+          };
+        });
+        setOpportunities(formattedData);
+      });
+  }, []);
+  
+
+  function formatPrice(price: any) {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price);
+  }
+
+
   return (
     <section>
       <h1 className={styles.title}>Oportunidades da semana</h1>
@@ -18,108 +59,25 @@ export default function Opportunity() {
           modules={[Navigation]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
+          {opportunities.map((opportunity) => (
+            <SwiperSlide key={opportunity.id}> 
+              <div className={styles.opportunity}>
+                <div className={styles.bg}  style={{ backgroundImage: `url(${opportunity.image})` }}>
+                  <p className={styles.sell}>Venda</p>
+                  <div className={styles.container_mobile}>
+                    <p className={styles.name}>{opportunity.name}</p> 
+                    <div className={styles.config}>
+                      <span style={{position: 'relative', left: '14px'}}>{opportunity.zone}{opportunity.zone_full} úteis</span> <span>{opportunity.suites} Suítes</span> 
+                    </div>
+                    <p className={styles.price}>{formatPrice(opportunity.price)}</p> 
                   </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
+                </div>
+                <div className={styles.location}>
+                  <span>{opportunity.city}</span> <span className={styles.view}>Ver imóvel</span>
                 </div>
               </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
-                  </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
-                </div>
-              </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
-                  </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
-                </div>
-              </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
-                  </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
-                </div>
-              </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
-                  </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
-                </div>
-              </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
-                  </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
-                </div>
-              </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </section>
--- src/components/Release.tsx
+++ src/components/Release.tsx
@@ -1,11 +1,61 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
 import styles from "../styles/Release.module.css";
+import api from "@/services/api";
+
+interface Release {
+  id: any;
+  price: any;
+  product_media: Array<{ url: string }>; 
+  image: any;
+  name: any;
+  zone: any;
+  zone_full: any;
+  suites: any;
+  city: any;
+}
 
 export default function Release() {
+  const [releases, setReleases] = useState<Release[]>([]);
+
+  useEffect(() => {
+    api
+      .get('/guest/products-status')
+      .then((response) => {
+        const formattedData = response.data.map((item: Release) => {
+          const imageUrl = item.product_media && item.product_media[0] ? item.product_media[0].url : '';
+          return {
+            ...item,
+            image: imageUrl,
+          };
+        });
+        setReleases(formattedData); // atualiza com os dados formatados
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 429) {
+          setTimeout(() => {
+            api.get('/guest/products-status')
+              .then((response) => {
+                setReleases(response.data)
+              })
+              .catch((error) => {
+                console.log(error)
+              })
+          }, 5000) 
+        }
+      })
+  }, []);
+
+  function formatPrice(price: any) {
+    return new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL",
+    }).format(price);
+  }
+
   return (
     <section>
       <h1 className={styles.title}>Pré - Lançamentos</h1>
@@ -18,108 +68,25 @@ export default function Release() {
           modules={[Navigation]}
           className="mySwiper"
         >
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
+          {releases.map((release) => (
+            <SwiperSlide key={release.id}>
+              <div className={styles.opportunity}>
+                <div className={styles.bg}  style={{ backgroundImage: `url(${release.image})` }}>
+                  <p className={styles.sell}>Venda</p>
+                  <div className={styles.container_mobile}>
+                    <p className={styles.name}>{release.name}</p> 
+                    <div className={styles.config}>
+                      <span style={{position: 'relative', left: '14px'}}>{release.zone}{release.zone_full} úteis</span> <span>{release.suites} Suítes</span> 
+                    </div>
+                    <p className={styles.price}>{formatPrice(release.price)}</p> 
                   </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
+                </div>
+                <div className={styles.location}>
+                  <span>{release.city}</span> <span className={styles.view}>Ver imóvel</span>
                 </div>
               </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
-                  </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
-                </div>
-              </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
-                  </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
-                </div>
-              </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
-                  </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
-                </div>
-              </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
-                  </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
-                </div>
-              </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className={styles.opportunity}>
-              <div className={styles.bg}>
-                <p className={styles.sell}>Venda</p>
-                <div className={styles.container_mobile}>
-                  <p className={styles.name}>Apartamento</p>
-                  <div className={styles.config}>
-                  <span>330m úteis</span> <span>4 Suítes</span>
-                  </div>
-                  <p className={styles.price}>R$ 14.500.000,00</p>
-                </div>
-              </div>
-              <div className={styles.location}>
-                <span>Balneário Camboriú</span> <span className={styles.view}>Ver imóvel</span>
-              </div>
-            </div>
-          </SwiperSlide>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </section>
