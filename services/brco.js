84c8dc6b52dd306c26988f14adf102dee71173a6
Luis Dandolini <luisfernandodandoliniduarte@gmail.com>
2023-04-24 10:12:44 -0300

    feat add requisions cache

--- services/brco.js
+++ services/brco.js
@@ -1,7 +1,40 @@
 const apiKey = 'LQCIT1QDVJVJ0KCG';
 
+let brco11Cache = {
+  value: null,
+  variation: null,
+  timestamp: null
+};
+
+let callCountBrco11 = 0;
+let currentDateBrco11 = new Date().toISOString().split('T')[0];
+
+const CACHE_DURATION_BRCO11 = 1000 * 60 * 60 * 6; // 6 horas em milissegundos
+
 export const fetchBrco11 = async () => {
+  const today = new Date().toISOString().split('T')[0];
+
+  if (currentDateBrco11 !== today) {
+    currentDateBrco11 = today;
+    callCountBrco11 = 0;
+  }
+
+  if (callCountBrco11 >= 4) {
+    console.warn('Limite de 4 chamadas por dia atingido');
+    return { value: null, variation: null };
+  }
+
   try {
+    const currentTime = new Date().getTime();
+
+    if (brco11Cache.timestamp && (currentTime - brco11Cache.timestamp) <= CACHE_DURATION_BRCO11) {
+      // Retorna os dados em cache se ainda estiverem válidos
+      return {
+        value: brco11Cache.value,
+        variation: brco11Cache.variation
+      };
+    }
+
     const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=BRCO11.SAO&apikey=${apiKey}`);
     const data = await response.json();
     const lastRefreshed = data['Meta Data']['3. Last Refreshed'];
@@ -11,9 +44,22 @@ export const fetchBrco11 = async () => {
     const brco11Value = data['Time Series (Daily)'][lastRefreshed]['4. close'];
     const brco11PreviousValue = data['Time Series (Daily)'][previousDayFormatted]['4. close'];
     const brco11Variation = brco11Value - brco11PreviousValue;
-    return { value: brco11Value, variation: brco11Variation };
+
+    const result = { value: brco11Value, variation: brco11Variation };
+
+    // Armazena os dados e a hora atual no cache
+    brco11Cache = {
+      value: result.value,
+      variation: result.variation,
+      timestamp: currentTime
+    };
+
+    callCountBrco11++;
+
+    return result;
   } catch (error) {
     console.error('Erro ao buscar o BRCO11:', error);
+    console.log(callCountBrco11)
     return { value: null, variation: null };
   }
 };
--- services/ifix.js
+++ services/ifix.js
@@ -1,7 +1,40 @@
+let ifixCache = {
+  value: null,
+  variation: null,
+  timestamp: null
+};
+
+const CACHE_DURATION = 1000 * 60 * 60 * 5; // 5 horas em milissegundos
+
 const apiKey = 'LQCIT1QDVJVJ0KCG';
 
+let callCount = 0;
+let currentDate = new Date().toISOString().split('T')[0];
+
 export const fetchIfix = async () => {
+  const today = new Date().toISOString().split('T')[0];
+
+  if (currentDate !== today) {
+    currentDate = today;
+    callCount = 0;
+  }
+
+  if (callCount >= 4) {
+    console.warn('Limite de 4 chamadas por dia atingido');
+    return { value: null, variation: null };
+  }
+
   try {
+    const currentTime = new Date().getTime();
+
+    if (ifixCache.timestamp && (currentTime - ifixCache.timestamp) <= CACHE_DURATION) {
+      // Retorna os dados em cache se ainda estiverem válidos
+      return {
+        value: ifixCache.value,
+        variation: ifixCache.variation
+      };
+    }
+
     const response = await fetch(`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IFIX&apikey=${apiKey}`);
     const data = await response.json();
     const lastRefreshed = data['Meta Data']['3. Last Refreshed'];
@@ -10,10 +43,22 @@ export const fetchIfix = async () => {
     const previousDayFormatted = previousDay.toISOString().split('T')[0];
     const ifixValue = data['Time Series (Daily)'][lastRefreshed]['4. close'];
     const ifixPreviousValue = data['Time Series (Daily)'][previousDayFormatted]['4. close'];
-    return {
+
+    const result = {
       value: ifixValue,
-      variation: ifixValue - ifixPreviousValue,
+      variation: ifixValue - ifixPreviousValue
     };
+
+    // Armazena os dados e a hora atual no cache
+    ifixCache = {
+      value: result.value,
+      variation: result.variation,
+      timestamp: currentTime
+    };
+
+    callCount++;
+
+    return result;
   } catch (error) {
     console.error('Erro ao buscar o IFIX:', error);
     return { value: null, variation: null };
