acf4a395c4d659e301efea4e0efd5143e82e6541
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-06-13 11:10:14 -0300

    feat page imovelUnico

---
+++ components/Imovel/ImovelUnico.tsx
@@ -0,0 +1,103 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
+import styles from '../../src/styles/imovel_css/ImovelUnico.module.css';
+import Icon from '@mdi/react';
+import { mdiMapMarkerRadius, mdiBedKing, mdiCarBack, mdiRulerSquare, mdiPlusThick, mdiMinusThick } from '@mdi/js';
+import { useState, useEffect } from "react";
+import { useMediaQuery } from 'react-responsive';
+import ImovelUnicoMobile from "./ImovelUnicoMobile";
+
+export default function ImovelUnico() {
+  const [isMobile, setIsMobile] = useState(false);
+  const isMobileQuery = useMediaQuery({ query: `(max-width: 767px)` });
+  const [openDescription, setOpenDescription] = useState(false);
+  const [openInfoAp, setOpenInfoAp] = useState(false);
+  const [openInfoEmp, setOpenInfoEmp] = useState(false);
+
+  const handleToggleDescription = () => setOpenDescription(!openDescription);
+  const handleToggleAp = () => setOpenInfoAp(!openInfoAp);
+  const handleToggleEmp = () => setOpenInfoEmp(!openInfoEmp);
+
+  useEffect(() => {
+    setIsMobile(isMobileQuery);
+  }, [isMobileQuery]);
+
+  if (isMobile) {
+    return <ImovelUnicoMobile />;
+  }
+
+  return(
+    <section id="imovel">
+      <Swiper navigation={true} modules={[Navigation]} className="mySwiper" slidesPerView={3} spaceBetween={10} loop={true}>
+        <SwiperSlide>
+          <div className={styles.img}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.img}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.img}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.img}></div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.img}></div>
+        </SwiperSlide>
+      </Swiper>
+
+      <div>
+        <div className={styles.container_infos}>
+          <div>
+            <p className={styles.imobile_code}>CA021</p>
+            <p className={styles.imobile_name}>Casa de alto padrão em condomínio fechado com 3 suítes e 2 vagas de garagem</p>  
+            <div className={styles.imobile_location}>
+              <p><span><Icon path={mdiMapMarkerRadius} size={.9} /></span> Praia Brava - Itajaí</p>
+              <button>Mapa</button>
+            </div>
+            <div className={styles.imobile_config}>
+              <p><span><Icon path={mdiBedKing} size={.9} /></span> 3 Quartos</p>
+              <p><span><Icon path={mdiCarBack} size={.9} /></span> 3 Vagas</p>
+              <p><span><Icon path={mdiRulerSquare} size={.9} /></span> 125 m</p>
+            </div>
+
+            <div className={styles.container_description}>
+              <div className={styles.tooltip} onClick={handleToggleDescription}>
+                <p>Descrição</p> 
+                {openDescription ? <Icon path={mdiMinusThick} size={.9} /> : <Icon path={mdiPlusThick} size={.9} />}
+              </div>
+                {openDescription && <div>Oi</div>}
+            </div>
+
+            <div className={styles.container_description}>
+              <div className={styles.tooltip} onClick={handleToggleAp}>
+                <p>Informações do Apartamento</p> 
+                {openInfoAp ? <Icon path={mdiMinusThick} size={.9} /> : <Icon path={mdiPlusThick} size={.9} />}
+              </div>
+                {openInfoAp && <div>Oi</div>}
+            </div>
+
+            <div className={styles.container_description}>
+              <div className={styles.tooltip} onClick={handleToggleEmp}>
+                <p>Informações do Empreendimento</p> 
+                {openInfoEmp ? <Icon path={mdiMinusThick} size={.9} /> : <Icon path={mdiPlusThick} size={.9} />}
+              </div>
+                {openInfoEmp && <div>Oi</div>}
+            </div>
+          </div>
+
+          <div>
+            <div className={styles.imobile_price}>
+              <p className={styles.price}>R$ <span>3.900.000,00</span></p>
+              <p>Condições de pagamento</p>
+              <button className={styles.information}>Quero mais informações</button>
+              <button className={styles.doubts}>Tirar dúvidas</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
